690c7de420bbd76abc39912c9f45b753f736e11b
marcsamuel2006 <tonemail@gmail.com>
2026-03-17 10:19:51 +0000

    mise à jour du code

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os #pour lire le systeme
+from dotenv import load_dotenv #pour charger les variables d'environnement
+# Charger les variables d'environnement à partir du fichier .env
+load_dotenv()
+#recuerer l url 
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+ 
 # définir l'URL de connexion à MariaDB
-# mysql://utilisateur:mot_de_passe@hote:port/nom_de_la_base
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/bemtech"
+
 # création du moteur SQL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 # créer une session locale pour effectuer des requêtes
@@ -17,9 +23,3 @@ def get_db():
         yield db
     finally:
         db.close()
-import os #pour lire le systeme
-from dotenv import load_dotenv #pour charger les variables d'environnement
-# Charger les variables d'environnement à partir du fichier .env
-load_dotenv()
-#recuerer l url 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") 
